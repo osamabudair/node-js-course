3b5d3598d47b07dddbc8df4fa77eeee29b0b9d37
osama budair <osama.budair04@gmail.com>
2026-08-03 21:58:33 +0300

    upload images

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const logger = require("./middlewares/logger");
 const { notFound, errorHandler } = require("./middlewares/errors");
 const dotenv = require("dotenv").config();
 const connectToDB = require('./config/db');
+const path = require("path");
 
 // Connection To Database
 connectToDB();
 
 // Init App
 const app = express();
+
+// static folder
+app.use(express.static(path.join(__dirname, "images")));
+
 
 //Apply Middlewares
 app.use(express.json());
@@ -23,6 +28,7 @@ app.use("/api/books", require("./routes/books"));
 app.use("/api/authors", require("./routes/authors"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
+app.use("/api/upload", require("./routes/upload"));
 app.use("/password", require("./routes/password"));
 
 // Error Handler Middleware
---
+++ test.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const imagesPath =path.join(__dirname, "images");
+
+console.log(__dirname);
+console.log(imagesPath);
+
